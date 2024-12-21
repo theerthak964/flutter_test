7ce3c4538e22b2cca0f51afa34f3eb616e0fddb4
Theertha K <theerthak964@gmail.com>
2024-12-21 12:43:59 +0530

    first commit

---
+++ lib/components/popupPayment.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:order_app_project/components/common_popup.dart';
+import 'package:order_app_project/components/commoncolor.dart';
+
+
+class PaymentSelect {
+  showpaymentSheet(
+      BuildContext context,
+      String areaId,
+      String areaName,
+      String cusid,
+      String date,
+      String time,
+      String ref,
+      String reason,
+      double baserate,
+      String branch_id,
+      double cashdisc_per,
+      double cashdisc_amt,
+      double tot_aftr_disc,
+      String frompage) {
+    Size size = MediaQuery.of(context).size;
+    String? payment_mode;
+    print("base rate..for insert...........$baserate");
+    CommonPopup salepopup = CommonPopup();
+    CommonPopup returnPop = CommonPopup();
+    return showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        child: const Text(
+                          'Cash',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: P_Settings.salewaveColor,
+                            textStyle: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold)),
+                        onPressed: () {
+                          payment_mode = "-2";
+                          if (frompage == "salecart") {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  salepopup.buildPopupDialog(
+                                      "sales",
+                                      context,
+                                      "Confirm your sale?",
+                                      areaId,
+                                      areaName,
+                                      cusid,
+                                      date,
+                                      time,
+                                      "",
+                                      "",
+                                      payment_mode!,
+                                      branch_id,
+                                      cashdisc_per,
+                                      cashdisc_amt,
+                                      tot_aftr_disc),
+                            );
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  returnPop.buildPopupDialog(
+                                      "return",
+                                      context,
+                                      "Confirm your order?",
+                                      areaId,
+                                      areaName,
+                                      cusid,
+                                      date,
+                                      time,
+                                      ref,
+                                      reason,
+                                      payment_mode!,
+                                      branch_id,
+                                      0.0,
+                                      0.0,
+                                      0.0),
+                            );
+                          }
+
+                          print(
+                              "payment mode...........$payment_mode...........$baserate");
+                        }),
+                    SizedBox(
+                      width: size.width * 0.06,
+                    ),
+                    ElevatedButton(
+                        child: const Text(
+                          'Credit',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: P_Settings.salewaveColor,
+                            textStyle: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold)),
+                        onPressed: () {
+                          payment_mode = "-3";
+                          if (frompage == "salecart") {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  salepopup.buildPopupDialog(
+                                      "sales",
+                                      context,
+                                      "Confirm your sale?",
+                                      areaId,
+                                      areaName,
+                                      cusid,
+                                      date,
+                                      time,
+                                      "",
+                                      "",
+                                      payment_mode!,
+                                      branch_id,
+                                      cashdisc_per,
+                                      cashdisc_amt,
+                                      tot_aftr_disc),
+                            );
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  returnPop.buildPopupDialog(
+                                      "return",
+                                      context,
+                                      "Confirm your order?",
+                                      areaId,
+                                      areaName,
+                                      cusid,
+                                      date,
+                                      time,
+                                      ref,
+                                      reason,
+                                      payment_mode!,
+                                      branch_id,
+                                      0.0,
+                                      0.0,
+                                      0.0),
+                            );
+                          }
+                          // print("payment mode...........$payment_mode");
+                        }),
+                  ],
+                ),
+                // SizedBox(
+                //   height: size.height * 0.03,
+                // ),
+                // ElevatedButton(
+                //     child: const Text('Done'),
+                //     style: ElevatedButton.styleFrom(
+                //         primary: Colors.green,
+                //         textStyle: TextStyle(
+                //             fontSize: 15,
+                //             fontWeight:
+                //                 FontWeight.bold)),
+                //     onPressed: () {
+                //       print(
+                //           "payment_mode...${payment_mode}");
+                //     }),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import device_info_plus
+import path_provider_foundation
+import printing
+import share_plus
+import shared_preferences_foundation
+import sqflite_darwin
+import url_launcher_macos
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
+}
